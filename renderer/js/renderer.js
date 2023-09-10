e82c194f123c45b4b5d2bc36c952658b5e2ac58d
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-09-10 13:21:15 +0530

    add functionality to display image name and image dimensions

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -13,9 +13,19 @@ function loadImage(e) {
         return;
     }
 
-    console.log('Success');
+    // Get original dimensions
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+    image.onload = function () {
+        widthInput.value = this.width;
+        heightInput.value = this.height;
+    };
+
+    form.style.display = 'block';
+    filename.innerText = file.name;
 }
 
+// Check allowed image file type
 function isFileImage(file) {
     const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg', 'image/tiff'];
     return file && acceptedImageTypes.includes(file['type']);
