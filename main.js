eb298cd4e61b71bcd74491afe10ff27fa38ff638
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-09-10 12:23:51 +0530

    createAboutWindow functionality added

--- main.js
+++ main.js
@@ -21,6 +21,18 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+// Create about window
+function createAboutWindow() {
+    const aboutWindow = new BrowserWindow({
+        title: 'About Image Resizers',
+        width: 300,
+        height: 300,
+        resizable: false
+    })
+
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
 // App is ready to serve
 app.whenReady().then(() => {
     createMainWindow()
@@ -36,15 +48,18 @@ app.whenReady().then(() => {
     })
 })
 
+const aboutMenuOption = {
+    label: 'About',
+    click: createAboutWindow
+};
+
 // Menu template
 const menu = [
     ...(isMac ? [
         {
             label: app.getName(),
             submenu: [
-                {
-                    label: 'About'
-                },
+                aboutMenuOption,
                 {
                     role: 'Quit'
                 }
@@ -58,9 +73,7 @@ const menu = [
         {
             label: 'Help',
             submenu: [
-                {
-                    label: 'About'
-                }
+                aboutMenuOption
             ]
         }
     ] : [])
